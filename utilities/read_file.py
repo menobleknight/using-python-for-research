904316cbb82887949ae41058399857862a993a8b
Ajay Dandge <60701544+menobleknight@users.noreply.github.com>
2021-04-06 09:48:51 +0530

    .

--- utilities/read_file.py
+++ utilities/read_file.py
@@ -2,16 +2,22 @@ def read_file(file_path):
     """
     Reads a file on github.com at given file_path and return contents of a file in form of str. 
     """
-    import urllib, bs4
-    text = []
+    import urllib.request, urllib.parse
     
     # if there are non-ascii characters in file_path
     file_path = urllib.parse.quote(file_path, safe="/:") # don't encode '/' and ':'
 
     with urllib.request.urlopen(file_path) as f:
-        for line in bs4.BeautifulSoup(f)('td'):
-            if len(line) > 0:
-                text.append(line.contents[0].strip())
+        text = f.read().decode('utf-8') # read() returns data as bytes, decode data to str using 'utf-8'
 
-    text = ' '.join(text)
     return text
+
+# file with single line
+
+file_path = "https://raw.githubusercontent.com/menobleknight/using-python-for-research/main/week-3-part-1/dna-translation/protein.txt"
+print(read_file(file_path))
+
+# file with multiple lines
+
+file_path = "https://raw.githubusercontent.com/menobleknight/using-python-for-research/main/week-3-part-2/books/english/shakespeare/A-Midsummer-Night's-Dream.txt"
+print(read_file(file_path))
